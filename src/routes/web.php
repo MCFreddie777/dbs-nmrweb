64b6a343b9cc273525961448ed0fea97787811af
František Gič <frantisek.gic@gmail.com>
2020-05-13 20:56:46 +0200

    Fix bug in router when wildcard caused other routes to be unreachable
    Signed-off-by: František Gič <frantisek.gic@gmail.com>

--- src/routes/web.php
+++ src/routes/web.php
@@ -36,11 +36,11 @@ Route::middleware('auth')->group(function () {
 
     Route::prefix('analyses')->group(function () {
         Route::get('/', 'AnalysesController@index');
-        Route::get('/{id}', 'AnalysesController@show');
         Route::middleware('role:laborant,admin')->group(function () {
             Route::get('/new', 'AnalysesController@create');
             Route::post('/', 'AnalysesController@store');
         });
+        Route::get('/{id}', 'AnalysesController@show');
     });
 
     Route::middleware('role:garant,admin')->group(function () {
